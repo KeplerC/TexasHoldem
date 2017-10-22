cada8b5872d64048375481fbac09b833500be603
Kaiyuan <chenkaiyuan@ucla.edu>
2017-10-22 10:54:02 -0700

    pythonver

--- texas.py
+++ texas.py
@@ -1,1 +1,126 @@
-#!/usr/bin/python
+
+# coding: utf-8
+
+# In[2]:
+
+SCORE_FOLD = 1 #if expected score is below two pairs, then fold
+
+#import libraries 
+import pandas as pd
+import tempfile
+import urllib
+
+
+
+# In[4]:
+
+#read training file from UCI dataset 
+#train_file = tempfile.NamedTemporaryFile()
+#urllib.request.urlretrieve("https://archive.ics.uci.edu/ml/machine-learning-databases/poker/poker-hand-training-true.data", train_file.name)
+
+CSV_COLUMNS = ["S1","C1","S2","C2","S3","C3","S4","C4", "S5","C5","Hand"]
+read_df = pd.read_csv("./poker-hand-training-true.data", names=CSV_COLUMNS, skipinitialspace=True)
+#read_df = pd.read_csv(train_file.name, names=CSV_COLUMNS, skipinitialspace=True)
+print(read_df)
+
+
+# In[5]:
+
+#generate training cases 
+#because we have only two cards available, we generate two combinations with corresponding result 
+
+import itertools
+
+#select two combinations 
+train_list = list(itertools.combinations(range(0,10,2), 2))
+print(train_list)
+
+frames = []
+for target in train_list:
+    local_train_list = [target[0], target[0]+1, target[1], target[1]+1, 10] #append the final result to our table 
+    local_data_frame = read_df.iloc[:, local_train_list]
+    local_data_frame.columns = ["S1","C1","S2","C2", "Hand"]
+    frames.append(local_data_frame)
+training_df = pd.concat(frames)
+print(training_df)
+
+
+# In[6]:
+
+train_labels = (training_df["Hand"].apply(lambda x: x > SCORE_FOLD)).astype(int)
+print(train_labels)
+
+
+# In[14]:
+
+import numpy as np
+trainX = training_df.iloc[:, 0:4].values
+trainY = train_labels.values
+
+learning_rate = 0.1
+training_epochs = 10000
+batch_size = 100
+display_step = 50
+m, n = trainX.shape
+#m: number of training samples 
+#n: number of features
+
+print(trainX.shape)
+print(trainY.shape)
+
+def fetch_batch(X_train, Y_train, batch_index, batch_size):
+    X_batch = X_train[batch_index*batch_size: (batch_index+1)*batch_size, :]
+    y_batch = Y_train[batch_index*batch_size: (batch_index+1)*batch_size]
+    return X_batch, y_batch
+    
+
+
+# In[15]:
+
+#tensorflow
+#do logistic regression 
+import tensorflow as tf
+import numpy as np
+
+
+#graph input
+x = tf.placeholder(tf.float32, [None, n])
+Y = tf.placeholder(tf.float32)
+
+#weights 
+W = tf.Variable(tf.zeros([n, 1]), name="weight")
+b = tf.Variable(np.random.randn(), name="bias")
+
+#model
+pred = tf.nn.softmax(tf.matmul(x, W) + b)
+
+#cost 
+cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*m)
+
+#optimizer is Gradient Descent
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+init = tf.global_variables_initializer()
+
+#training
+with tf.Session() as sess:
+    sess.run(init)
+    
+    for epoch in range(training_epochs):
+        average_cost = 0
+        total_batch = int(m / batch_size)
+        for i in range(total_batch):
+            batch_x, batch_y = fetch_batch(trainX, trainY, i, batch_size)
+            _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, Y: batch_y})
+            average_cost += c/total_batch
+        
+        if(epoch+1) % display_step == 0:
+            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(average_cost))
+            
+    print("Optimization Finished!")
+
+
+# In[ ]:
+
+
+
